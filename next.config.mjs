52a0512c243b6bd7f42c5dbc14b5efdd0b63a443
Federico <federico@Mac-mini-de-Technicoders.local>
2024-09-17 13:40:40 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, max-age=0, must-revalidate, value'
+                    }
+                ]
+            }
+        ]
+    },
+}
 
 export default nextConfig;
